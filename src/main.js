678ecf951d3ed86601897e21b50adb550b960243
satriabha <theblacksteel99@gmail.com>
2025-05-27 17:37:11 +0700

    do something

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
   const paths = req.path.split('/');
-  const env = dotenv.config()['parsed']
+  const env = dotenv.config()['parsed'];
 
   if (paths[1] === 'api' && paths[2] === 'file' && paths.length == 4) {
     console.log('Logging by console.log');
@@ -14,31 +14,32 @@ export default async ({ req, res, log, error }) => {
     const client = new Client();
 
     client
-        .setEndpoint(env.APPWRITE_FUNCTION_API_ENDPOINT)
-        .setProject(env.APPWRITE_FUNCTION_PROJECT_ID)
-        .setKey(env.APPWRITE_FUNCTION_API_KEY) // Your secret API key
-    ;
+      .setEndpoint(env.APPWRITE_FUNCTION_API_ENDPOINT)
+      .setProject(env.APPWRITE_FUNCTION_PROJECT_ID)
+      .setKey(env.APPWRITE_FUNCTION_API_KEY); // Your secret API key
 
-    const storage = new Storage(client)
-    const db = new Databases(client)
+    log(process.env.APPWRITE_ENDPOINT);
+    log(process.env.APPWRITE_URL);
+    log(process.env.APPWRITE_FUNCTION_PROJECT_ID);
+    log(process.env.APPWRITE_API_KEY);
+    log(process.env.APPWRITE_FUNCTION_API_KEY);
+    const storage = new Storage(client);
+    const db = new Databases(client);
 
-    const name = paths[3]
+    const name = paths[3];
 
+    const docsList = await db
+      .listDocuments(env.APPWRITE_DB_ID, env.APPWRITE_COLLECTION_FILES_ID, [
+        Query.equal('filename', [`${name}.csv`]),
+      ])
+      .catch((e) => e);
 
-    const docsList = await db.listDocuments(
-        env.APPWRITE_DB_ID,
-        env.APPWRITE_COLLECTION_FILES_ID,
-        [
-            Query.equal('filename', [`${name}.csv`])
-        ]
-    ).catch(e => e)
+    const fileID = docsList.documents[0]['fileID'];
 
-    const fileID = docsList.documents[0]['fileID'] 
-    
-    const file = await storage.getFileDownload(env.APPWRITE_BUCKET_ID, fileID)
+    const file = await storage.getFileDownload(env.APPWRITE_BUCKET_ID, fileID);
 
-    var enc = new TextDecoder("utf-8");
-    const csv = enc.decode(file)
+    var enc = new TextDecoder('utf-8');
+    const csv = enc.decode(file);
 
     return res.json({
       fileContent: csv,
