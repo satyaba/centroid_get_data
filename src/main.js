072f315b6dd17157067083a8032b89457f299c2a
satriabha <theblacksteel99@gmail.com>
2025-05-27 17:22:45 +0700

    do something

--- src/main.js
+++ src/main.js
@@ -1,51 +1,51 @@
-import { Client, Databases, Storage, Query } from "node-appwrite";
+import { Client, Databases, Storage, Query } from 'node-appwrite';
+import * as dotenv from 'dotenv';
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
-  console.log("Logging by console.log")
-  log("Logging by log()")
-  const client = new Client()
-  
-  client
-        .setEndpoint(process.env.APPWRITE_URL)
-        .setProject(process.env.APPWRITE_PROJECT_ID)
-        .setKey(req.headers['x-appwrite-key'] ?? process.env.APPWRITE_API_KEY) // Your secret API key
+  const paths = req.path.split('/');
+  const env = dotenv.config()['parsed']
+
+  if (paths[1] === 'api' && paths[2] === 'file' && paths.length == 4) {
+    console.log('Logging by console.log');
+    log('Logging by log()');
+    const client = new Client();
+
+    client
+        .setEndpoint(env.APPWRITE_URL)
+        .setProject(env.APPWRITE_PROJECT_ID)
+        .setKey(env.APPWRITE_API_KEY) // Your secret API key
     ;
 
-  log("Client: ", client)
-
-  const paths = req.path.split("/")
-
-  // The req object contains the request data
-  if (paths[1] === "api" && paths[2] ==="file" && paths.length == 4) {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
     const storage = new Storage(client)
     const db = new Databases(client)
 
+    const name = paths[3]
+
+
     const docsList = await db.listDocuments(
-        process.env.APPWRITE_DB_ID,
-        process.env.APPWRITE_COLLECTION_FILES_ID,
+        env.APPWRITE_DB_ID,
+        env.APPWRITE_COLLECTION_FILES_ID,
         [
-            Query.equal('filename', [`${paths[3]}.csv`])
+            Query.equal('filename', [`${name}.csv`])
         ]
-    ).catch(e => error("Error fetching document: ", e))
+    ).catch(e => e)
 
     const fileID = docsList.documents[0]['fileID'] 
     
-    const file = await storage.getFileDownload(process.env.APPWRITE_BUCKET_ID, fileID)
+    const file = await storage.getFileDownload(env.APPWRITE_BUCKET_ID, fileID)
 
     var enc = new TextDecoder("utf-8");
-    const csv = enc.decode(file);
+    const csv = enc.decode(file)
 
     return res.json({
-        fileContent: csv
+      fileContent: csv,
     });
   }
 
   return res.json({
-    error: "An error occured"
+    error: 'An error occured',
   });
 };
